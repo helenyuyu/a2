fb9c0c27701e0048b1c110163562eae65caa6366
Jun Takahashi <jtakahas@princeton.edu>
2015-04-01 05:30:19 -0400

    import all data

--- regression.R
+++ regression.R
@@ -1,5 +1,6 @@
 #Set the working directory to 1 level below 'data'
-setwd("/Users/jun/Documents/424/a2")
-sample_data <- read.table("data/intersected_final_chr1_cutoff_20_train_revised.bed", sep="\t")
+#setwd("/Users/jun/Documents/424/a2")
+training_data <- read.table("data/intersected_final_chr1_cutoff_20_train_revised.bed", sep="\t")
+test_data <- read.table("data/intersected_final_chr1_cutoff_20_test.bed", sep="\t")
+sample_data <- read.table("data/intersected_final_chr1_cutoff_20_sample.bed", sep="\t")
 
-sample_data
