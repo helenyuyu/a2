7bac7f5c73965cddfc263a14239019b46501dabb
Jun Takahashi <jtakahas@princeton.edu>
2015-04-01 05:05:19 -0400

    initial commit regression.R

---
+++ regression.R
@@ -0,0 +1,5 @@
+#Set the working directory to 1 level below 'data'
+setwd("/Users/jun/Documents/424/a2")
+sample_data <- read.table("data/intersected_final_chr1_cutoff_20_train_revised.bed", sep="\t")
+
+sample_data
