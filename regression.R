76bbd3d7ae2ccfe44fbd3f5dcf5d836b462db16e
Jun Takahashi <jtakahas@princeton.edu>
2015-04-01 23:40:37 -0400

    fixed betareg

--- regression.R
+++ regression.R
@@ -3,40 +3,24 @@ setwd("/Users/jun/Documents/424/a2")
 #training_data <- read.table("data/intersected_final_chr1_cutoff_20_train_revised.bed", sep="\t")
 #test_data <- read.table("data/intersected_final_chr1_cutoff_20_test.bed", sep="\t")
 #sample_data <- read.table("data/intersected_final_chr1_cutoff_20_sample.bed", sep="\t")
+#> known <- read.table("known")
 
 library(betareg)
 
-fill_in_row <- function(row) {
-  count <- 0;
-  sum <- 0;
-  for (i in row) {
-    if (is.nan(i) == FALSE) {
-      count <- count + 1;
-      sum <- sum + i;
-    }
-  }
-  avg <- sum / count;
-  
-  for (i in 1:(length(row))) {    
-    if (is.nan(row[[i]]) == TRUE) {
-      row[i] = avg;
-    }
-  }
-  return (row);
-}
-
-#for (i in 1:nrow(training_data)) {
-for (i in 1:10) {
-    row <- fill_in_row(training_data[i,5:37])
-
-  print (row)
-}
+#known <- read.table("known")
+#unknown_target <- read.table("unknown_target")
+#unknown_vector <- read.table("unknown_vector")
 
 
-
-#gy <- betareg()
+gy <- betareg(V34 ~ V1+V2+V3+V4+V5+V6+V7+V8+V9+V10+
+    V11+V12+V13+V14+V15+V16+V17+V18+V19+V20+V21+V22+V23+V24+V25+
+    V26+V27+V28+V29+V30+V31+V32+V33, data = known)
 #summary(gy)
+predicted_target <- predict(gy, type="response", newdata=unknown_vector)
+RMSE <- sqrt(mean((predicted_target-unknown_target)^2))
 
-
-
+meanPred <- mean(predicted_target)
+SSRes <- sum((predicted_target - unknown_target)^2)
+SSTot <- sum((predicted_target - meanPred)^2)
+RSquared <- 1 - (SSRes)/(SSTot)
 
